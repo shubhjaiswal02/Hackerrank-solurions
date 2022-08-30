86ba1c29eb6f4f629444945ff2effbb950f9f029
shubhjaiswal02 <112515178+shubhjaiswal02@users.noreply.github.com>
2022-08-31 01:55:16 +0530

    Update and rename Java Date and Time problem to Java_Date_and_Time_problem.java
    This problem shows the use of Calendar Class.

--- Java_Date_and_Time_problem.java
+++ Java_Date_and_Time_problem.java
@@ -11,17 +11,7 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'findDay' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. INTEGER month
-     *  2. INTEGER day
-     *  3. INTEGER year
-     */
-
+    
     public static String findDay(int month, int day, int year) {
      String daytosee[] = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
         Calendar cal = Calendar.getInstance();
